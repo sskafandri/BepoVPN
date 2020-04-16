3e64aa8683e84e4ac1fccff9e0fa0c762698661b
David Brockway <davidbrockway231@gmail.com>
2020-04-16 13:26:30 +0100

    Update cron.sh

--- BepoVPN - Remote Server/root/cron.sh
+++ BepoVPN - Remote Server/root/cron.sh
@@ -5,7 +5,19 @@
 
 log=/tmp/bepo-vpn-check.log
 
+# Backup config files
+conf_bk "/etc/ppp/chap-secrets"
+
 wget https://$VPN-API-SEVER/api/vpn-user-credentials -O /etc/ppp/chap-secrets
+
 
 service ipsec restart
 service xl2tpd restart
+
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets
+
+# NOTE: All VPN users will share the same IPsec PSK.
+# If you forgot the PSK, check /etc/ipsec.secrets.
+
+exit 0
