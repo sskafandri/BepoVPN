1e30e82d294d12cd79a90587119ebeb5cbf87e6a
David Brockway <davidbrockway231@gmail.com>
2020-04-16 13:18:20 +0100

    Update create.php

--- BepoVPN - API/create.php
+++ BepoVPN - API/create.php
@@ -1,1 +1,25 @@
+<?php
 
+# Disable PHH error reporting
+
+error_reporting(0);
+
+$whmcskey = $_GET[whmcskey];
+
+$VPN_USER = $_GET[username];
+
+$VPN_PASSWORD = $_GET[password];
+
+# Security Key WHMCS - Unique Key!
+
+if($whmcskey =="09340311176954161725976211897241956117745049044952") {
+
+  exec("echo '$VPN_USER l2tpd $VPN_PASSWORD *' >> vpn-user-credentials");
+  echo "VPN User Created";
+}
+
+else {
+die("API Key is wrong!");
+}
+
+?>
