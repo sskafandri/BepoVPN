5d443b355d7af3721a9200b5ea54f482ba9437c0
David Brockway <davidbrockway231@gmail.com>
2020-04-16 13:21:25 +0100

    Update terminate.php

--- BepoVPN - API/terminate.php
+++ BepoVPN - API/terminate.php
@@ -6,15 +6,15 @@ error_reporting(0);
 
 $whmcskey = $_GET[whmcskey];
 
-$username = $_GET[username];
+$VPN_USER = $_GET[username];
 
-$password = $_GET[password];
+$VPN_PASSWORD = $_GET[password];
 
 # Security Key WHMCS - Unique Key!
 
 if($whmcskey =="09340311176954161725976211897241956117745049044952") {
 
-echo shell_exec("sed '/$username/d'  vpn-user-credentials > vpn-user-credentials.tmp 2>&1");
+echo shell_exec("sed -i '/^\"$VPN_USER\" /d' vpn-user-credentials > vpn-user-credentials.tmp 2>&1");
 
 echo exec("mv vpn-user-credentials.tmp vpn-user-credentials");
 echo exec("chmod 600 vpn-user-credentials");
