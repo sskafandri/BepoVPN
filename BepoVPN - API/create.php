616c82029bf1c7165780fe144b90d952f899657e
David Brockway <davidbrockway231@gmail.com>
2020-04-16 13:24:38 +0100

    Update create.php

--- BepoVPN - API/create.php
+++ BepoVPN - API/create.php
@@ -13,7 +13,7 @@ $VPN_PASSWORD = $_GET[password];
 # Security Key WHMCS - Unique Key!
 
 if($whmcskey =="09340311176954161725976211897241956117745049044952") {
-
+  
   exec("echo '$VPN_USER l2tpd $VPN_PASSWORD *' >> vpn-user-credentials");
   echo "VPN User Created";
 }
